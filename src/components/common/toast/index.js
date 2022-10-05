d1800926a1b99a9de18ccf94e8b3f752004717b4
蔺杳01 <1712132179@qq.com>
2022-10-05 19:21:59 +0800

    添加了toast插件的注释以便以后再理解

--- src/components/common/toast/index.js
+++ src/components/common/toast/index.js
@@ -2,10 +2,13 @@ import Toast from './toast'
 
 export default {
     install(Vue) {
+        //创建组件构造器
         const toastConstructor = Vue.extend(Toast)
+        //new的方式，根据组件构造器，可以创建出来一个 组件对象
         const toast = new toastConstructor()
-
+        // 将组件对象，手动挂载到某一个元素上
         toast.$mount(document.createElement("div"))
+        // toast.$el对应的就是div
         document.body.appendChild(toast.$el)
 
         Vue.prototype.$toast = toast
